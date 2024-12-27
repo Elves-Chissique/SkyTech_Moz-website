6b349e6dbae9018286a385759e6858c5325a4e8b
Elves Chissique <pedrochissique@hotmail.com>
2024-12-27 07:32:29 +0200

    updates

--- Assets/Scripts/app.js
+++ Assets/Scripts/app.js
@@ -30,3 +30,37 @@ function chamarComercio(){
    bodyII.style.display = 'flex'
    bodyI.style.display = 'none'
 }
+/*Configuração de BodyI*/
+// Area de animação dos paragrafos
+// Escuta o evento DOMContentLoaded corretamente
+window.addEventListener('DOMContentLoaded', () => {
+   // Verificando se o ScrollTrigger foi carregado corretamente
+   if (typeof ScrollTrigger !== 'undefined') {
+       // Seleciona os elementos
+       const Descr = document.querySelector('.Descr'); // Um único elemento
+       const firtParag = document.querySelectorAll('.firt-parag'); // NodeList com múltiplos elementos
+
+       // Animação em múltiplos elementos ".firt-parag" (se existirem)
+       gsap.to(firtParag, {
+           opacity:1,
+           marginLeft:50,
+           scale: 1.2,
+           duration: .5,
+           stagger: 0.1, // Animação em cascata para cada elemento
+           scrollTrigger: {
+               trigger: Descr, // Define o trigger para o elemento ".Descr"
+               start: 'top center', // Quando o topo de .Descr atingir o centro da tela
+               end: 'bottom center', // Quando o fundo de .Descr atingir o centro da tela
+               scrub: true, // Faz a animação acompanhar o movimento do scroll
+           }
+       });
+   } else {
+       console.warn("ScrollTrigger não foi carregado corretamente.");
+   }
+});
+
+
+// Configurando BodyII o play rate do video de background 
+var videoBg = window.document.querySelector('.video-bg')
+
+videoBg.playbackRate = 0.5
